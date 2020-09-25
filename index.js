791c5a48c0caf1cecc38283912f7c32cc9c2682f
Thomas Wright <twwright@gmail.com>
2020-09-25 18:28:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,15 +3,13 @@ let dodger = document.getElementById('dodger');
 function moveDodgerLeft() {
   let leftNumbers = dodger.style.left.replace('px', '');
   let left = parseInt(leftNumbers, 10)
-  if (left > 0) dodger.style.left = `${left - 1}px`;
+  if (left > 0) { dodger.style.left = `${left - 1}px`; };
 }
 
 function moveDodgerRight() {
   let leftNumbers = dodger.style.left.replace('px', '');
   let left = parseInt(leftNumbers, 10)
-  if (left > 0) {
-    dodger.style.left = `${left + 1}px`
-  }
+  if (left > 0) { dodger.style.left = `${left + 1}px`; };
 }
 
 document.addEventListener('keydown', function(e) {
