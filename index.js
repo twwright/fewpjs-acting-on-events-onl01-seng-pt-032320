eb16fb55d93b5c4156226f337a1105d8ce1d5529
Thomas Wright <twwright@gmail.com>
2020-09-25 18:26:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ function moveDodgerLeft() {
 }
 
 function moveDodgerRight() {
-  let rightNumbers = dodger.style.left.replace('px', '');
+  let leftNumbers = dodger.style.left.replace('px', '');
   let left = parseInt(leftNumbers, 10)
   if (left > 0) {
-    dodger.style.left = `${left - 1}px`
+    dodger.style.left = `${left + 1}px`
   }
 }
