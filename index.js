9af300aaef9f32876c142865683f8a709d73cb09
Thomas Wright <twwright@gmail.com>
2020-09-25 18:29:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function moveDodgerLeft() {
 function moveDodgerRight() {
   let leftNumbers = dodger.style.left.replace('px', '');
   let left = parseInt(leftNumbers, 10);
-  if (left > 0) { dodger.style.left = `${left + 1}px`; }
+  if (left < 0) { dodger.style.left = `${left + 1}px`; }
 }
 
 document.addEventListener('keydown', function(e) {
